2dfb8586e4343d6f1f36117d7b0f2fd2788789bf
anniluo <anniluo@metropolia.fi>
2018-09-24 12:44:33 +0300

    libGDX with kotlin setup

---
+++ app/src/main/java/com/example/anni/riggedpongsensorproject/Launcher.kt
@@ -0,0 +1,13 @@
+package com.example.anni.riggedpongsensorproject
+
+import android.os.Bundle
+import com.badlogic.gdx.backends.android.AndroidApplication
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration
+
+class Launcher : AndroidApplication() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val config = AndroidApplicationConfiguration()
+        initialize(LibGdxDemo(), config)
+    }
+}
--- app/src/main/java/com/example/anni/riggedpongsensorproject/MainActivity.kt
+++ app/src/main/java/com/example/anni/riggedpongsensorproject/MainActivity.kt
@@ -1,12 +1,25 @@
 package com.example.anni.riggedpongsensorproject
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val libGDXLauncherBtn : Button = findViewById<Button>(R.id.libGDXLauncherBtn)
+
+        libGDXLauncherBtn.setOnClickListener {
+            launchlibGDX()
+        }
+    }
+
+    private fun launchlibGDX() {
+        val intent = Intent(this, Launcher::class.java)
+        startActivity(intent)
     }
 }
